6b2d086ded41ec9afafd426ee1f3f89d3e7497dd
ramdio12 <luib.diomar@gmail.com>
2023-06-30 12:01:30 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/hooks/Constant.js
@@ -0,0 +1,7 @@
+export const CONSTANTS = {
+    algodToken : '',
+    baseServer : 'https://testnet-api.algonode.cloud',
+    port : '443',
+    APP_ID : 252236904,
+    explorer : 'https://testnet.algoexplorer.io/application/252236904' 
+ }
